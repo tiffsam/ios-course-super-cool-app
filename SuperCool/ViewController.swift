ff109c10904aba5f26b8a05244da2b11ea881370
Ticktock <smu@ticktock.local>
2016-05-29 14:34:11 +0800

    all files that were not committed

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolBg: UIImageView!
+    @IBOutlet weak var CoolButton: UIButton!
+    @IBOutlet weak var CoolLogo: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func MakeMeLessUnCool(sender: AnyObject) {
+        
+        CoolLogo.hidden = false
+        CoolBg.hidden = false
+        CoolButton.hidden = true
+    }
 
 }
 
